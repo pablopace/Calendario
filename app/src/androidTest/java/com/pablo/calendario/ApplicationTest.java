ae2d864335d2947a4f443502aa87a5c56bf4cf73
pablopace <pablopace@users.noreply.github.com>
2018-04-03 17:20:07 -0300

    Update ApplicationTest.java

--- app/src/androidTest/java/com/pablo/calendario/ApplicationTest.java
+++ app/src/androidTest/java/com/pablo/calendario/ApplicationTest.java
@@ -9,5 +9,10 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+        
+        
+        
+        
+        //esto dese la web
     }
 }
